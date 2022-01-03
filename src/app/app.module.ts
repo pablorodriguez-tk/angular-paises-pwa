cb8eaf99ad909e5408206a0d1c7505bcf135fe59
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-01-02 21:50:33 -0300

    agrego componentes y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisComponent } from './pages/pais/pais.component';
+import { PaisesComponent } from './pages/paises/paises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaisComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule
