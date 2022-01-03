19a3d4ecb98de03448b8743d5a2c31afe5282d37
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-01-03 20:33:12 -0300

    agrego estructura pais

--- src/app/pages/pais/pais.component.ts
+++ src/app/pages/pais/pais.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaisInterface } from 'src/app/interfaces/pais.interface';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-pais',
   templateUrl: './pais.component.html',
-  styleUrls: ['./pais.component.css']
+  styleUrls: ['./pais.component.css'],
 })
 export class PaisComponent implements OnInit {
+  pais: PaisInterface;
+  constructor(
+    public paisesService: PaisesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(id);
+    this.paisesService.getPaisPorId(id).then((pais) => {
+      if (!pais) {
+        return this.router.navigateByUrl('/');
+      }
+      this.pais = pais;
+      console.log(pais);
+    });
   }
-
 }
--- src/app/services/paises.service.ts
+++ src/app/services/paises.service.ts
@@ -23,4 +23,16 @@ export class PaisesService {
         });
     });
   }
+
+  getPaisPorId(id: string) {
+    if (this.paises.length > 0) {
+      //hay paises en el arreglo
+      const pais = this.paises.find((p) => p.cca3 === id);
+      return Promise.resolve(pais);
+    }
+    return this.getPaises().then((paises) => {
+      const pais = this.paises.find((p) => p.cca3 === id);
+      return Promise.resolve(pais);
+    });
+  }
 }
