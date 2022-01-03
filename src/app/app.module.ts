20774813ab1349f5bf3e2e555d844b8098e7ef9c
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-01-02 22:07:11 -0300

    rutas de la aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PaisComponent } from './pages/pais/pais.component';
 import { PaisesComponent } from './pages/paises/paises.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PaisComponent,
-    PaisesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PaisComponent, PaisesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
